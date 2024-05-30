afe2bc9f9616fd4c339aedc07c031a7bce598a97
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-30 13:16:33 +0300

    add app and config setting

---
+++ analytic/views.py
@@ -0,0 +1,10 @@
+import http.client
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request,'analytic/index.html')
