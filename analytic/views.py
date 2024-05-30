629918d3defea5545c795cc3a837a95b9dbed50d
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-30 15:37:38 +0300

    working with templates

--- analytic/views.py
+++ analytic/views.py
@@ -4,20 +4,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 from pandas.io import json
+from .models import Data
 
 # Create your views here.
 
 def home(request):
     print(request)
     if (request.method=='POST'):
-        file_cvs=request.FILES['file']
-        df=pd.read_csv(file_cvs)
-        json_recoreds=df.reset_index().to_json(orient='records')
-        data=[]
-        data=json.ujson_loads(json_recoreds)
-        print(data)
+
+        try:
+            priveious_data=Data.objects.all()
+            priveious_data.delete() # all priveious  data deleted
+            file_cvs=request.FILES['file'] # chacke the post request is CSV file
+            df=pd.read_csv(file_cvs)  # read that csv file
+            json_recoreds=df.reset_index().to_json(orient='records') # convert that file  in jeson format with index number integaer
+            data=[] # create empty list
+            data=json.ujson_loads(json_recoreds) # add that file in the list in ordere to extracte each
+            print(data)
+            for d in data:
+                name=d['property_name']
+                price=d['property_price']
+                rent=d['property_price']
+                emi=d['emi']
+                tax=d['tax']
+                exp=d['other_exp']
+                dt=Data(name=name,price=price,rent=rent,emi=emi,tax=tax,exp=exp)
+                dt.save()
+
+
+        except:
+            return HttpResponse('select file')
+
+
 
     else:
         print('this is get method')
+    data = Data.objects.all()
+    context={
+        'datas':data
+    }
 
-    return render(request,'analytic/index.html',)
+    return render(request,'analytic/index.html',context)
