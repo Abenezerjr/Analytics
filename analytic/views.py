21b8d0703cd6c296dbf1d1c72b091a9094d85bb7
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-30 14:29:16 +0300

    working with pandas

--- analytic/views.py
+++ analytic/views.py
@@ -2,9 +2,22 @@ import http.client
 
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+from pandas.io import json
 
 # Create your views here.
 
-
 def home(request):
-    return render(request,'analytic/index.html')
+    print(request)
+    if (request.method=='POST'):
+        file_cvs=request.FILES['file']
+        df=pd.read_csv(file_cvs)
+        json_recoreds=df.reset_index().to_json(orient='records')
+        data=[]
+        data=json.ujson_loads(json_recoreds)
+        print(data)
+
+    else:
+        print('this is get method')
+
+    return render(request,'analytic/index.html',)
